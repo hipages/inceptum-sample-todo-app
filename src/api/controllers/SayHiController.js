2997d39991f728332e44ea60624f9d1df85f1a98
Paolo Ragone <pragone@gmail.com>
2017-03-07 16:05:06 +1100

    Added sayHi sample controller

---
+++ src/api/controllers/SayHiController.js
@@ -0,0 +1,12 @@
+
+class SayHiController {
+  * sayHi() {
+    return { message: this.salute };
+  }
+}
+
+SayHiController.autowire = {
+  salute: '#application.salutation'
+}
+
+module.exports = SayHiController;
